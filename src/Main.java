d400160f2710143a06a27448d6b85c04d4794b08
ZionBlade <wernerct@mail.uc.edu>
2022-10-01 23:44:00 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+        int number = rand.nextInt();
+        int guess = 0;
+
+        System.out.print("\nGuess a number between 1 and 10");
+    }
+}
